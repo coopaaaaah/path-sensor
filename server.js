be6054e8a4f849717c6dfb84466d84ed0a06d209
yubi <dmcooper2412@gmail.com>
2020-11-21 22:23:54 +0000

    small logic to send out path data

--- server.js
+++ server.js
@@ -1,9 +1,36 @@
 const gpio = require('rpi-gpio');
 
-const PIR_PIN = 24;
+const PIR_I_PIN = 24;
+const PIR_O_PIN = 40;
+
+let paths = [];
 
 gpio.on('change', function (channel, value) {
-	console.log(`Channel ${channel} value is now ${value}`);
+	
+	if (channel == 24 && value == true && !paths.include(24)) {
+		paths.push(24);
+	} else if (channel == 40 && value == true && !paths.include(40)) {
+		paths.push(40);
+	}
+
+	if (paths.include(24) && paths.include(40)) {
+		// post	
+		const payload = {
+			node: 'beta',
+			start: paths[0],
+			end: path[1]
+		};
+		
+		axois
+			.post('http://beta.local/api/path', payload)
+			.catch(error => {
+				console.error(error);
+			});
+	
+		// reset data
+		paths = [];
+	}
 });
 
-gpio.setup(PIR_PIN, gpio.DIR_IN, gpio.EDGE_BOTH);
+gpio.setup(PIR_I_PIN, gpio.DIR_IN, gpio.EDGE_BOTH);
+gpio.setup(PIR_O_PIN, gpio.DIR_IN, gpio.EDGE_BOTH);
