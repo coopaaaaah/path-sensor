f3202d1ac21b73e2fbeebf5d93078c0fe1f92e4e
yubi <dmcooper2412@gmail.com>
2020-11-22 13:10:25 +0000

    modified post obj

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const gpio = require('rpi-gpio');
 
 const PIR_I_PIN = 24;
@@ -5,24 +6,32 @@ const PIR_O_PIN = 40;
 
 let paths = [];
 
+gpio.setup(PIR_I_PIN, gpio.DIR_IN, gpio.EDGE_BOTH);
+gpio.setup(PIR_O_PIN, gpio.DIR_IN, gpio.EDGE_BOTH);
 gpio.on('change', function (channel, value) {
-	
-	if (channel == 24 && value == true && !paths.include(24)) {
+
+	if (channel == 24 && value == true && !paths.includes(24)) {
 		paths.push(24);
-	} else if (channel == 40 && value == true && !paths.include(40)) {
+	} else if (channel == 40 && value == true && !paths.includes(40)) {
 		paths.push(40);
 	}
 
-	if (paths.include(24) && paths.include(40)) {
+	if (paths.includes(24) && paths.includes(40)) {
 		// post	
 		const payload = {
-			node: 'beta',
+			node: 'alpha',
 			start: paths[0],
-			end: path[1]
+			end: paths[1]
 		};
-		
-		axois
-			.post('http://beta.local/api/path', payload)
+	
+		const options = {
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		};
+
+		axios
+			.post('http://beta/api/path', payload, options)
 			.catch(error => {
 				console.error(error);
 			});
@@ -32,5 +41,3 @@ gpio.on('change', function (channel, value) {
 	}
 });
 
-gpio.setup(PIR_I_PIN, gpio.DIR_IN, gpio.EDGE_BOTH);
-gpio.setup(PIR_O_PIN, gpio.DIR_IN, gpio.EDGE_BOTH);
